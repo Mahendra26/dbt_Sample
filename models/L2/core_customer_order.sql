b55a5e7fc6d465dcba6d6dc6a6dce2eed2723842
mahendra <mahendrasisodiya26@gmail.com>
2020-06-03 14:09:35 +0530

    Renamed Models directory

---
+++ models/L2/core_customer_order.sql
@@ -0,0 +1,37 @@
+with orders as (
+
+    select * from {{ ref('stg_orders') }}
+
+),
+
+customers as (
+
+	select * from {{ ref('stg_customers') }}
+
+
+),
+
+renamed as (
+
+    select
+        customer_id,
+
+        min(order_date) as first_order,
+        max(order_date) as most_recent_order,
+        count(order_id) as number_of_orders
+    from orders
+
+    group by 1
+
+),
+
+final as (
+
+    select *
+    from customers
+
+    join renamed using (customer_id)
+
+)
+
+select * from final
